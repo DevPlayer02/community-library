ecaf2629e09bd1f346db23b92b459aa1d2c47e4e
DevPlayer02 <contato.giovanicf@gmail.com>
2025-03-25 21:43:02 -0300

    Feat: create user repository inserted

---
+++ src/repositories/user.repositories.js
@@ -0,0 +1,35 @@
+import db from '../config/database.js'
+
+db.run(`
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGET PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL,
+            email TEXT UNIQUE NOT NULL,
+            password TEX NOT NULL,
+            avatar TEXT
+        )
+    `)
+
+function createUserRepository(newUser) {
+    return new Promise((res, rej) => {
+        const { username, email, password, avatar } = newUser
+        db.run(
+            `
+                INSERT INTO users (username, email, password, avatar)
+                VALUES (?, ?, ?, ?)
+            `,
+            [username, email, password, avatar], 
+            (err) => {
+                if (err) {
+                    rej(err)
+                } else {
+                    res({ message: 'User created' })
+                }
+            }
+        );
+    });
+}
+
+export default {
+    createUserRepository
+}
