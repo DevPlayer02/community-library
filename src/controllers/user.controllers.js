07e65566fc901e953d0867a62368efd037edc9b2
DevPlayer02 <contato.giovanicf@gmail.com>
2025-03-26 19:26:10 -0300

    Feat: Adding bcrypt and nodemon

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -2,9 +2,10 @@ import userService from "../service/user.services.js";
 
 async function createUserController(req, res) {
     const newUser = req.body;
+
     try {
         const user = await userService.createUserService(newUser);
-        res.status(201).send({user});
+        return res.status(201).send({user});
     } catch (err) {
         return res.status(400).send(err.message);
     }
