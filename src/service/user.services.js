5baddc6c17ae82dcda10f36d0556508f4baf3052
DevPlayer02 <contato.giovanicf@gmail.com>
2025-04-01 18:47:12 -0300

    Feat: Adding environment variable

--- src/service/user.services.js
+++ src/service/user.services.js
@@ -11,8 +11,7 @@ async function createUserService(newUser) {
     const passHash = await bcrypt.hash(newUser.password, 10);
     const user = await userRepository.createUserRepository({...newUser, password: passHash});
     if(!user) throw new Error("Error creating user!");
-    const userByEmail = await userRepository.findUserByEmailRepository(user.email);
-    const token = generateJWT(userByEmail.id);
+    const token = generateJWT(user.id);
     return token;
 }
 
