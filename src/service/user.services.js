80777988b4e45a8f00fc6371d7b6e442125174cf
DevPlayer02 <contato.giovanicf@gmail.com>
2025-04-01 18:27:14 -0300

    Feat: Creating authentication JWT

--- src/service/user.services.js
+++ src/service/user.services.js
@@ -1,4 +1,5 @@
 import userRepository from "../repositories/user.repositories.js";
+import { generateJWT } from "./auth.service.js";
 import bcrypt from 'bcrypt';
 
 async function createUserService(newUser) {
@@ -10,7 +11,9 @@ async function createUserService(newUser) {
     const passHash = await bcrypt.hash(newUser.password, 10);
     const user = await userRepository.createUserRepository({...newUser, password: passHash});
     if(!user) throw new Error("Error creating user!");
-    return user;
+    const userByEmail = await userRepository.findUserByEmailRepository(user.email);
+    const token = generateJWT(userByEmail.id);
+    return token;
 }
 
 async function findAllUsersService() {
