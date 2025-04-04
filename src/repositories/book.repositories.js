44d80801b2e636ec07f95b2cd2082323d15e85ec
DevPlayer02 <contato.giovanicf@gmail.com>
2025-04-03 21:52:08 -0300

    Feat: Adding query strings

--- src/repositories/book.repositories.js
+++ src/repositories/book.repositories.js
@@ -102,10 +102,27 @@ function deleteBookRepository(bookId) {
     })
 }
 
+function searchBookRepository(search) {
+    return new Promise((resolve, reject) => {
+        db.all(`
+                SELECT * FROM books
+                WHERE title LIKE ? OR author LIKE ?
+            `, [`%${search}%`, `%${search}%`], (err, rows) => {
+                if(err) {
+                    reject(err)
+                } else {
+                    resolve(rows)
+                }
+            }
+        )
+    })
+}
+
 export default { 
     createBookRepositoty,
     findAllBooksRepository,
     findBookByIdRepository,
     updateBookRepository,
-    deleteBookRepository
+    deleteBookRepository,
+    searchBookRepository
 }
